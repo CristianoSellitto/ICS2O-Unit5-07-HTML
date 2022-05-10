4d21123e484e2850470d7dcc49a8d362f43b17c8
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-10 17:26:03 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-07-HTML
+// May 10 2022
 
 'use strict'
 /**
- * Check service worker.
+ * Adds the integer the user chose added up to all the numbers from one to the integer
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-07-HTML/",
   })
 }
 
@@ -16,5 +16,20 @@ if (navigator.serviceWorker) {
  * Alerts "Hello, World!"
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  const integer = document.getElementById("integer").value
+  var text = "1"
+  var counter = 1
+  var answer
+
+  if (integer > 0) {
+    answer = 1
+    for (let count = 1; count < integer; count++) {
+      counter++
+      text = text + " + " + counter
+      answer = answer + counter
+    }
+    document.getElementById("answer").innerHTML = "<h5>" + answer + "</h5><br><h5>is the answer of " + text + "</h5>"
+  } else {
+    document.getElementById("answer").innerHTML = "<h5>Error: This integer is negative.</h5>"
+  }
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = "/respitory"
+var GHPATH = "/ICS2O-Unit5-07-HTML"
 var APP_PREFIX = "gppwa_"
 var VERSION = "version_001"
 var URLS = [
